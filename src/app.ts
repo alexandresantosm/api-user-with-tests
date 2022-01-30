75be2b93788682ed9715f0d5ff64c915da83867a
alexandresantosm <amenezes031@gmail.com>
2022-01-30 16:19:49 -0300

    create route GET /users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { NextFunction, Response, Request } from 'express';
 import 'express-async-errors';
 import 'dotenv/config';
+import { routes } from './routes/routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
