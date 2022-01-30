f8cad02c6beba74dce86841893450302984d4dc2
alexandresantosm <amenezes031@gmail.com>
2022-01-30 18:20:10 -0300

    create new user route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
 import { Request, Response, Router } from 'express';
+import { createUserFactory } from '../modules/createUser/CreateUserFactory';
 
 const routes = Router();
 
-routes.get('/users', (request: Request, response: Response) =>
-  response.status(200).send('Access users route.')
+routes.post('/users', (request: Request, response: Response | any) =>
+  createUserFactory().handle(request, response)
 );
 
 export { routes };
